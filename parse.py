453ad3ea754de523aee8983d349f573d8211e290
Krzysztof Rakowski <krzysztof@rakowski.pl>
2014-04-24 16:51:56 +0200

    debug added closes #12

--- parse.py
+++ parse.py
@@ -8,6 +8,13 @@ from smsapi.responses import ApiError
 import pycurl, cStringIO
 import settings
 
+try:
+    DEBUG = settings.DEBUG
+except NameError:
+    DEBUG = False
+
+
+
 # log in
 c = pycurl.Curl()
 c.setopt(c.COOKIEFILE, '')
@@ -121,18 +128,21 @@ if message.get('napad') != None:
 # send sms
 
 if message_text != '':
-    try:
-        smsapi = SmsAPI()
-        smsapi.set_username(settings.SMSAPI_LOGIN)
-        smsapi.set_password(settings.SMSAPI_PASS)
-        smsapi.service('sms').action('send')
-        if settings.SMSAPI_PRO == True:
-            smsapi.set_from(settings.SMSAPI_PRO_FROM)
-        else:
-            smsapi.set_eco(True)
-        for phone in settings.PHONE_NUMBERS:
-            smsapi.set_content(message_text.encode('utf-8'))
-            smsapi.set_to(phone)
-            smsapi.execute()
-    except ApiError, e:
-        print 'ERROR: %s - %s' % (e.code, e.message)
+    if DEBUG == True:
+        print message_text
+    else:
+        try:
+            smsapi = SmsAPI()
+            smsapi.set_username(settings.SMSAPI_LOGIN)
+            smsapi.set_password(settings.SMSAPI_PASS)
+            smsapi.service('sms').action('send')
+            if settings.SMSAPI_PRO == True:
+                smsapi.set_from(settings.SMSAPI_PRO_FROM)
+            else:
+                smsapi.set_eco(True)
+            for phone in settings.PHONE_NUMBERS:
+                smsapi.set_content(message_text.encode('utf-8'))
+                smsapi.set_to(phone)
+                smsapi.execute()
+        except ApiError, e:
+            print 'ERROR: %s - %s' % (e.code, e.message)
