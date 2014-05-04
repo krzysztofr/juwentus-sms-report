4d4071e6a6fe00dcf4889510bb8cd322ef2ce056
Krzysztof Rakowski <krzysztof@rakowski.pl>
2014-05-04 18:15:04 +0200

    moved main block to main()

--- parse.py
+++ parse.py
@@ -7,30 +7,34 @@ from settings import default as settings
 
 from juwparser import parse, get_log, get_last_time
 
-try:
-    DEBUG = settings['DEBUG']
-except NameError:
-    DEBUG = False
 
+def main():
+    try:
+        DEBUG = settings['DEBUG']
+    except NameError:
+        DEBUG = False
 
-message_text = parse(html=get_log(settings=settings), last_time=get_last_time(), settings=settings)
+    message_text = parse(html=get_log(settings=settings), last_time=get_last_time(), settings=settings)
 
-if message_text != '':
-    if DEBUG:
-        print message_text
-    else:
-        try:
-            smsapi = SmsAPI()
-            smsapi.set_username(settings['SMSAPI_LOGIN'])
-            smsapi.set_password(settings['SMSAPI_PASS'])
-            smsapi.service('sms').action('send')
-            if settings.SMSAPI_PRO:
-                smsapi.set_from(settings['SMSAPI_PRO_FROM'])
-            else:
-                smsapi.set_eco(True)
-            for phone in settings['PHONE_NUMBERS']:
-                smsapi.set_content(message_text.encode('utf-8'))
-                smsapi.set_to(phone)
-                smsapi.execute()
-        except ApiError, e:
-            print 'ERROR: %s - %s' % (e.code, e.message)
+    if message_text != '':
+        if DEBUG:
+            print message_text
+        else:
+            try:
+                smsapi = SmsAPI()
+                smsapi.set_username(settings['SMSAPI_LOGIN'])
+                smsapi.set_password(settings['SMSAPI_PASS'])
+                smsapi.service('sms').action('send')
+                if settings.SMSAPI_PRO:
+                    smsapi.set_from(settings['SMSAPI_PRO_FROM'])
+                else:
+                    smsapi.set_eco(True)
+                for phone in settings['PHONE_NUMBERS']:
+                    smsapi.set_content(message_text.encode('utf-8'))
+                    smsapi.set_to(phone)
+                    smsapi.execute()
+            except ApiError, e:
+                print 'ERROR: %s - %s' % (e.code, e.message)
+
+if __name__ == "__main__":
+    main()
