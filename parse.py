f985e2beb3af96aa836736b1092974f960c49828
Krzysztof Rakowski <krzysztof@rakowski.pl>
2014-06-05 00:22:57 +0200

    it is possible to disable parsing with stop.py file, refs #23

--- parse.py
+++ parse.py
@@ -2,7 +2,10 @@
 
 
 from settings import default as settings
-
+try:
+    from stop import dont_parse
+except ImportError:
+    dont_parse = False
 from juwparser import parse, get_log, get_last_time
 
 from importlib import import_module
@@ -15,15 +18,18 @@ argparser.add_argument('action', action='store', help="action (parse, getlog)", 
 action = argparser.parse_args().action
 
 if action == 'parse':
-    message_text = parse(html=get_log(settings=settings), last_time=get_last_time(), settings=settings)
+    if dont_parse:
+        print 'Parsing disabled with stop.dont_parse.'
+    else:
+        message_text = parse(html=get_log(settings=settings), last_time=get_last_time(), settings=settings)
 
-    if message_text != '':
-        sendermod = {}
-        for sender in settings['SENDERS']:
-            try:
-                sendermod[sender] = import_module('senders.%s' % sender)
-                sendermod[sender].send(message=message_text, settings=settings)
-            except ImportError:
-                print 'No such sender: %s' % sender
+        if message_text != '':
+            sendermod = {}
+            for sender in settings['SENDERS']:
+                try:
+                    sendermod[sender] = import_module('senders.%s' % sender)
+                    sendermod[sender].send(message=message_text, settings=settings)
+                except ImportError:
+                    print 'No such sender: %s' % sender
 elif action == 'getlog':
     print get_log(settings=settings)
